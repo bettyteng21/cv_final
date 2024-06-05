f4fa112542854dc6b155c61df0bf28e009c7c175
betty <betty.teng21@gmail.com>
2024-06-05 14:33:35 +0800

    PSNR: 51.73178

--- gmc.py
+++ gmc.py
@@ -2,36 +2,44 @@ import numpy as np
 import cv2
 import os
 
-# Divide an image into non-overlapping blocks of the specified size.
 def write_mmap(image, block_size, input_value, input_idx):
+    # 將傳入的model值寫入model map
     height, width = image.shape[:2]
-
     model_map_path = os.path.join('model_map', f'm_{input_idx:03}.txt')
     f = open(model_map_path, 'a')
-
     for y in range(0, height, block_size):
         for x in range(0, width, block_size):
             f.write(str(input_value)+'\n')
     f.close()
 
 
-# for non-bounding-box blocks
 def motion_compensate_for_nbb(blocks, ref0_image, ref1_image, target_idx):
     compensated_blocks = []
     for block,(y,x) in blocks:
-        # Find corresponding blocks in reference images
+        # 從target img block的座標(y,x)取出相同位置的ref0 & ref1 image block
         ref0_block = ref0_image[y:y+block.shape[0], x:x+block.shape[1]]
         ref1_block = ref1_image[y:y+block.shape[0], x:x+block.shape[1]]
         
         # Apply feature matching for motion compensation
         compensated_block = feature_matching_for_nbb(block, ref0_block, ref1_block, target_idx)
         compensated_blocks.append((compensated_block, (y, x)))
+
     return compensated_blocks
 
 
-# for non-bounding-box blocks
 def feature_matching_for_nbb(blk_target, blk_ref0, blk_ref1, target_idx):
-    # Initiate SURF detector
+    '''
+    真正主要做matching的fuction
+    @param target_idx: 為了後面write_mmap知道要開啟哪個檔案
+
+    model說明:
+    model 1: 使用ref0 block, 經過affine/projection transformation得到結果
+    model 2: 使用ref1 block, 經過affine/projection transformation得到結果
+    model 3: 直接使用ref0 block
+    model 4: 直接使用ref1 block
+    '''
+
+    # Step 1: 用surf找出target, ref0, ref1各自的特徵
     surf = cv2.xfeatures2d.SURF_create(300)
     kp_target, des_target = surf.detectAndCompute(blk_target,None)
     kp_ref0, des_ref0 = surf.detectAndCompute(blk_ref0,None)
@@ -39,11 +47,14 @@ def feature_matching_for_nbb(blk_target, blk_ref0, blk_ref1, target_idx):
 
     if des_target is None or (des_ref0 is None and des_ref1 is None):
         if des_ref0 is None:
+            write_mmap(blk_ref1, 16, 4, target_idx) # write model 4
             return blk_ref1
         else:
+            write_mmap(blk_ref0, 16, 3, target_idx) # write model 3
             return blk_ref0
 
-    # Initiate bf matcher
+
+    # Step 2: 用bf做matching
     bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
     matches0, matches1 = [],[]
     if des_ref0 is not None:
@@ -54,36 +65,47 @@ def feature_matching_for_nbb(blk_target, blk_ref0, blk_ref1, target_idx):
         matches1 = bf.match(des_target, des_ref1) 
         matches1 = sorted(matches1, key=lambda x: x.distance)
 
-    # Calculate total distance for matches0 and matches1
+
+    # Step 3: 將剛剛match的距離加總，看是ref0還是ref1的距離較小，較小的那個代表跟target比較相似
     if matches0 is not None:
         total_distance0 = sum([m.distance for m in matches0])
     if matches1 is not None:
         total_distance1 = sum([m.distance for m in matches1])
 
-    total_distance0 = sum([m.distance for m in matches0])
-    total_distance1 = sum([m.distance for m in matches1])
-
-    # Choose the matches with the lower total distance (higher similarity)
     if total_distance0 < total_distance1:
         chosen_matches = matches0
         kp_ref = kp_ref0
         blk_ref = blk_ref0
+        ref_select = 0
     else:
         chosen_matches = matches1
         kp_ref = kp_ref1
         blk_ref = blk_ref1
+        ref_select = 1
 
+
+    # Step 4: 取出比較相似的那張ref的點
     dst_pts = np.float32([kp_target[m.queryIdx].pt for m in chosen_matches]).reshape(-1, 1, 2)
     src_pts = np.float32([kp_ref[m.trainIdx].pt for m in chosen_matches]).reshape(-1, 1, 2)
     
-    if(len(dst_pts) > 8):
-        # calculate homography matrix if there's enough points
-        M, mask = cv2.estimateAffinePartial2D(src_pts, dst_pts)
-        compensated_block = cv2.warpAffine(blk_ref, M, (blk_ref0.shape[1], blk_ref0.shape[0]), borderMode=cv2.BORDER_REPLICATE, flags=cv2.INTER_CUBIC)
-        write_mmap(compensated_block, 16, 1, target_idx)
+
+    # Step 5: 計算projection
+    if len(dst_pts) > 8:
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        compensated_block = cv2.warpPerspective(blk_ref, M, (blk_ref0.shape[1], blk_ref0.shape[0]), borderMode=cv2.BORDER_REPLICATE, flags=cv2.INTER_CUBIC)
+
+        if ref_select == 0:
+            write_mmap(compensated_block, 16, 1, target_idx) # write model 1
+        else: # ref_select==1
+            write_mmap(compensated_block, 16, 2, target_idx) # write model 2
         
     else:
+        # 當點不夠的時候就不計算，直接用model 3 or 4
         compensated_block = blk_ref
-        write_mmap(compensated_block, 16, 2, target_idx)
+
+        if ref_select == 0:
+            write_mmap(compensated_block, 16, 3, target_idx) # write model 3
+        else: # ref_select==1
+            write_mmap(compensated_block, 16, 4, target_idx) # write model 4
 
     return compensated_block
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import pandas as pd
 from gmc import motion_compensate_for_nbb
 from select_block import select_blocks
 
-# Divide an image into non-overlapping blocks of the specified size.
 def divide_into_blocks(image, block_size):
+    # 將圖片切成以block_size為單位的blocks
     height, width = image.shape[:2]
     blocks = []
     for y in range(0, height, block_size):
@@ -18,14 +18,17 @@ def divide_into_blocks(image, block_size):
             blocks.append((block, (y,x)))
     return blocks
 
-# Paste the blocks back to an image
+
 def reconstruct_image_from_blocks(blocks, image_shape):
+    # 利用blocks裡的座標(y,x)，貼回去一張空白的圖，最後輸出一張合成圖
     compensated_image = np.full(image_shape, 80, dtype=np.uint8)
     for block, (y, x) in blocks:
         block_h, block_w = block.shape[:2]
-        # Ensure the block fits within the image dimensions
+        
         if y + block_h > image_shape[0] or x + block_w > image_shape[1]:
+            # 當超出圖片範圍時，resize成可以fit進去圖片的大小
             block = cv2.resize(block, (min(block_w, image_shape[1]-x), min(block_h, image_shape[0]-y)), interpolation=cv2.INTER_CUBIC)
+
         compensated_image[y:y+block_h, x:x+block_w] = block
 
     return compensated_image
@@ -61,7 +64,7 @@ def main():
             # skip the ones that are labeled (X)
             continue
         
-        # Step: Load img
+        # Step 1: Load img and files
         target, ref0, ref1 = int(target), int(ref0), int(ref1)
         print('\nCurrent processing order '+str(idx)+', target frame '+str(target))
 
@@ -77,43 +80,35 @@ def main():
         ref0_img = cv2.imread(ref0_img_path, cv2.IMREAD_GRAYSCALE)
         ref1_img = cv2.imread(ref1_img_path, cv2.IMREAD_GRAYSCALE)
 
-        # Step: apply motion model (gmc.py)
+        # Step 2: global motion compensation (gmc.py), get a list of compensated blocks
         compensated_blocks = []
-
-        # Step-1: gmc for background (non-bounding-box)
         blocks = divide_into_blocks(target_img, 400)
         temp_blocks = motion_compensate_for_nbb(blocks, ref0_img, ref1_img, target)
         compensated_blocks.extend(temp_blocks)
 
+        # Step 3: construct the compensated image
         compensated_image = reconstruct_image_from_blocks(compensated_blocks, target_img.shape)
-
         cv2.imwrite(os.path.join(args.output_path, f'{target:03}.png'), compensated_image)
 
-        # Step: Select 13,000 blocks (select_block.py)
+        # Step 4: Select 13,000 blocks of the top psnr (select_block.py)
         compensated_blocks = divide_into_blocks(compensated_image, 16)
         original_blocks = divide_into_blocks(target_img, 16)
         selected_blocks = select_blocks(compensated_blocks, original_blocks, num_blocks)
         
-        # ##############################################
-        # eval
+        # Step 5: eval for printing current psnr
         mask = np.array(selected_blocks).astype(bool)
         assert np.sum(mask) == 13000, 'The number of selection blocks should be 13000'
-
-
         s = compensated_image.reshape(2160//16, 16, 3840//16, 16).swapaxes(1, 2).reshape(-1, 16, 16).astype(int)
         g = target_img.reshape(2160//16, 16, 3840//16, 16).swapaxes(1, 2).reshape(-1, 16, 16).astype(int)
-        
         s = s[mask]
         g = g[mask]
         assert not (s == g).all(), "The prediction should not be the same as the ground truth"
-
         mse = np.sum((s-g)**2)/s.size
         psnr_curr = 10*np.log10((255**2)/mse)
         psnr_list.append(psnr_curr)
         print('Current psnr= '+str(psnr_curr))
-        # ###############################################
 
-        # Step: output selection map: s_xxx.txt
+        # Step 6: output selection map: s_xxx.txt
         output_path = os.path.join(args.output_path, f's_{target:03}.txt')
         smap_file = open(output_path,'w')
         for s in selected_blocks:
